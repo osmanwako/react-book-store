d0c45dea062046600e056a2645b282427250af5c
OsmanFP <osmanwako411@gmail.com>
2023-05-17 00:47:46 +0300

    Fix eslinter errors

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,25 +1,25 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-    books: [],
-    total: 0,
+  books: [],
+  total: 0,
 };
 
 export const Bookslice = createSlice({
-    name: 'bookstore',
-    initialState,
-    reducers: {
-        addbook: (state, { payload }) => {
-            const book = { ...payload, progress: 0 };
-            state.books = [...state.books, { ...book }];
-            state.total += 1;
-        },
-        removebook: (state, action) => {
-            const id = action.payload;
-            state.books = state.books.filter((book) => book.id !== id);
-            state.total -= 1;
-        },
+  name: 'bookstore',
+  initialState,
+  reducers: {
+    addbook: (state, { payload }) => {
+      const book = { ...payload, progress: 0 };
+      state.books = [...state.books, { ...book }];
+      state.total += 1;
     },
+    removebook: (state, action) => {
+      const id = action.payload;
+      state.books = state.books.filter((book) => book.id !== id);
+      state.total -= 1;
+    },
+  },
 });
 
 export const { addbook, removebook } = Bookslice.actions;
